791d052cac1f5df240bdd09deb542cf432503476
euiyounghwang <marieuig@gmail.com>
2023-08-17 20:37:53 -0500

    Initial Setup

---
+++ Python/Search-indexing-script.py
@@ -0,0 +1,7 @@
+
+def run():
+    print("Starting..")
+
+
+if __name__ == "__main__":
+    run()
---
+++ Python/docker-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+
+# python -m venv .venv
+# source .venv/bin/activate
+
+# poetry config virtualenvs.in-project true
+# poetry init
