507b5a3ec36a049e07cbce58230b7d42e6709580
euiyounghwang <marieuig@gmail.com>
2023-08-18 23:53:04 -0500

    Java Source initial commit

---
+++ Java/demo/src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * java -cp ./demo-1.0-SNAPSHOT.jar com.example.App
+ * java -jar ./demo/target/java-indexing-1.0-SNAPSHOT.jar
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
