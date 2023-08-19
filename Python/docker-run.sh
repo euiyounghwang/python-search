f010dcf4d019f9a46de34c64a6205e0508d0f5e1
euiyounghwang <marieuig@gmail.com>
2023-08-18 21:31:59 -0500

    Dockerfile Poetry

--- Python/docker-run.sh
+++ Python/docker-run.sh
@@ -6,7 +6,6 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo $SCRIPTDIR
 
-
 #docker run -rm -it --name a-test fn-search:run
 
 docker run --rm --platform linux/amd64 -it -d \
